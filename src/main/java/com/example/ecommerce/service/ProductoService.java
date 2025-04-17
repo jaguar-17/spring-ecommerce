0ecaf146c0cc08d8c0d095556bae52a90888355c
leandro <leandro@git.com>
2025-04-17 01:51:36 -0500

    crear el repository y las clases de servicio y el metodo show de ProductoController

---
+++ src/main/java/com/example/ecommerce/service/ProductoService.java
@@ -0,0 +1,15 @@
+package com.example.ecommerce.service;
+
+import com.example.ecommerce.model.Producto;
+
+import java.util.Optional;
+
+public interface ProductoService {
+    public Producto save(Producto producto);
+
+    public Optional<Producto> get(Integer id);
+
+    public void update(Producto producto);
+
+    public void delete(Integer id);
+}
