60d4c7436cea2951c16fdbd9a5429fe5b9a390e7
leandro <leandro@git.com>
2025-04-17 11:17:10 -0500

    implementar la funcionalidad de mostrar imagen para el administrador

--- src/main/java/com/example/ecommerce/controller/ProductoController.java
+++ src/main/java/com/example/ecommerce/controller/ProductoController.java
@@ -71,14 +71,12 @@ public class ProductoController {
 
     @PostMapping("/update")
     public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
+        Producto p = new Producto();
+        p = productoService.get(producto.getId()).get();
+
         if (file.isEmpty()){ // editamos el producto pero no cambiamos la imagen
-            Producto p = new Producto();
-            p = productoService.get(producto.getId()).get();
             producto.setImagen(p.getImagen());
         }else { // cuando se edita tambien la imagen
-            Producto p = new Producto();
-            p = productoService.get(producto.getId()).get();
-
             // eliminar si no es la imagen por defecto
             if (!p.getImagen().equals("default.jpg")){
                 uploadFileService.deleteImage(p.getImagen());
@@ -87,7 +85,7 @@ public class ProductoController {
             String nombreImagen = uploadFileService.saveImage(file);
             producto.setImagen(nombreImagen);
         }
-
+        producto.setUsuario(p.getUsuario());
         productoService.update(producto);
         return "redirect:/productos";
     }
